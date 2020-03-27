f53f8acfba9cb0f0f661e52e045c5701e5031b83
Jesus <jesusfeli54@gmail.com>
2020-03-26 19:09:28 -0500

    Se modificó el boton borrar para que borre como es debido

--- calculadora/calculator.py
+++ calculadora/calculator.py
@@ -1,30 +1,34 @@
 from tkinter import *
 
+
 class Interfaz:
     def __init__(self, ventana):
-        #Crear la ventana raiz con titulo
+        # Crear la ventana raiz con titulo
         self.ventana = ventana
         self.ventana.title("Calculadora")
-        self.ventana.iconbitmap("C:/Users/jesus/OneDrive/Escritorio/Mi Espacio/codigos/estructura de datos/proyecto_cuarentena/pruebas/calculadora.ico")
+        self.ventana.iconbitmap(
+            "C:/Users/jesus/OneDrive/Escritorio/Mi Espacio/codigos/estructura de datos/proyecto_cuarentena/pruebas/calculadora.ico")
         self.ventana.resizable(False, False)
         frame = Frame(self.ventana, width=248, height=219, background="#F7F7F7", padx=5, pady=5)
         frame.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
 
-        #Tablero
-        self.tablero = Text(self.ventana, state="disabled", width=27, height=3, background="gray83", foreground="black", font = ("Helvetica",12))
+        # Tablero
+        self.tablero = Text(self.ventana, state="disabled", width=27, height=3, background="gray83", foreground="black",
+                            font=("Helvetica", 12))
         self.tablero.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
 
-        #Operacion que se mostrará en tablero
+        # Operacion que se mostrará en tablero
         self.operation = ""
 
-        #Botones
+        # Botones
         boton1 = self.Crear_Boton_Numeros(7)
-        boton1.place(x= 7, y= 70)
+        boton1.place(x=7, y=70)
         boton2 = self.Crear_Boton_Numeros(8)
-        boton2.place(x= 68, y= 70)
+        boton2.place(x=68, y=70)
         boton3 = self.Crear_Boton_Numeros(9)
         boton3.place(x=129, y=70)
-        boton4 = Button(self.ventana, text=(u"\u232B"), width="7", height=2, command=lambda: self.click((u"\u232B"), False), background= "#FD6868")  # Borrar
+        boton4 = Button(self.ventana, text=(u"\u232B"), width="7", height=2,
+                        command=lambda: self.click((u"\u232B"), False), background="#FD6868")  # Borrar
         boton4.place(x=191, y=70)
         boton5 = self.Crear_Boton_Numeros(4)
         boton5.place(x=7, y=113)
@@ -50,39 +54,42 @@ class Interfaz:
         boton15.place(x=129, y=199)
         boton16 = self.Crear_Boton_Operaciones("-")
         boton16.place(x=191, y=199)
-        boton17 = Button(self.ventana, text="=", width="33", height=2, command=lambda: self.click("=", False), background="#99A4A4")
-        boton17.place(x=8, y=242)
+        boton17 = Button(self.ventana, text="=", width="16", height=2, command=lambda: self.click("=", False),
+                         background="#99A4A4")
+        boton17.place(x=6, y=242)
+        boton18 = Button(self.ventana, text="AC", width="16", height=2, command=lambda: self.click("AC", False),
+                         background="#FD6868")
+        boton18.place(x=129, y=242)
 
     def Crear_Boton_Numeros(self, valor, escribir=True, ancho=7, alto=2):
-        return Button(self.ventana, text=valor, width=ancho, height=alto, command=lambda: self.click(valor, escribir), background= "#C6CBCB", font = ("Helvetica",9))
+        return Button(self.ventana, text=valor, width=ancho, height=alto, command=lambda: self.click(valor, escribir),
+                      background="#C6CBCB", font=("Helvetica", 9))
 
     def Crear_Boton_Operaciones(self, valor, escribir=True, ancho=7, alto=2):
-        return Button(self.ventana, text=valor, width=ancho, height=alto, command=lambda: self.click(valor, escribir),background="#FFB300",font = ("Helvetica",9))
+        return Button(self.ventana, text=valor, width=ancho, height=alto, command=lambda: self.click(valor, escribir),
+                      background="#FFB300", font=("Helvetica", 9))
 
     def click(self, texto, escribir):
-        #si es uno de los botones de Borrar o Igual (no deben mostrarce por tablero)
-        self.operation = re.sub(u"\u00F7", "/", self.operation)  # sub remplaza "u"\u00F7" por "/" y lo guarda en el mismo string
+        # si es uno de los botones de Borrar o Igual (no deben mostrarce por tablero)
+        self.operation = re.sub(u"\u00F7", "/",self.operation)  # sub remplaza "u"\u00F7" por "/" y lo guarda en el mismo string
         if not escribir:
-            if texto== "=" and self.operation!="" and not self.operation[0] == "*" and not self.operation[0] == "/":
+            if texto == "=" and self.operation != "" and not self.operation[0] == "*" and not self.operation[0] == "/":
                 resultado = str(eval(self.operation))
-                self.operation= resultado
+                self.operation = resultado
                 self.Limpiar()
                 self.Mostrar(resultado)
-
+            elif texto == "AC":  # si es borrar
+                self.operation = ""
+                self.Limpiar()
             elif texto == u"\u232b":  # si es borrar
-                self.operation= self.operation[:-1]
+                self.operation = self.operation[:-1]
                 self.Limpiar()
                 self.Mostrar(self.operation)
-                print(self.operation)
-
-                #self.Limpiar()
-                #self.operation = ""
-
-        #Sino, si se muestra por tablero
+        # Sino, si se muestra por tablero
         else:
             if self.operation == "":
                 self.Limpiar()
-                self.operation+=str(texto)
+                self.operation += str(texto)
                 self.Mostrar(texto)
             else:
                 self.operation += str(texto)
@@ -100,6 +107,7 @@ class Interfaz:
         self.tablero.configure(state="disabled")
         return
 
-root=Tk()
-calculo=Interfaz(root)
+
+root = Tk()
+calculo = Interfaz(root)
 root.mainloop()
